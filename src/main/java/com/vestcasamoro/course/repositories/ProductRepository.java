8979796d9c9a3dcf428f2eefef6fe6c9fee86292
jesusapmoro <jesusapmoro@gmail.com>
2022-02-15 15:28:50 -0300

    Product

---
+++ src/main/java/com/vestcasamoro/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.vestcasamoro.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vestcasamoro.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
